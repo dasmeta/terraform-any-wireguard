efd424856b4ed24eee04cbc1a023cca65b88e975
Tigran Muradyan <tigran@dasmeta.com>
2023-10-10 15:33:14 +0400

    feat(DMVP-2721): cleanup

--- modules/aws/main.tf
+++ modules/aws/main.tf
@@ -61,7 +61,3 @@ resource "aws_eip" "this" {
   domain   = "vpc"
   instance = aws_instance.this.id
 }
-# resource "aws_eip_association" "this" {
-#   instance_id   = aws_instance.this.id
-#   allocation_id = aws_eip.this.id
-# }
